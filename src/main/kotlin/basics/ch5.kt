a2c305ba7db39a042de11df96f42d0dab41d7106
abubaker <mohammad_abubaker@yahoo.com>
2020-12-28 02:06:29 +0500

    Completed Chapter #5

--- src/main/kotlin/basics/ch5.kt
+++ src/main/kotlin/basics/ch5.kt
@@ -111,11 +111,75 @@ fun main(){
     }
 
     // Returning Values
+    val numberName = when (number){
+        2 -> "Two"
+        3 -> "Three"
+        8 -> "Eight"
+        10 -> "Ten"
+        else -> {
+            println("Unknown Number")
+            "Unknown"
+        }
+    }
+    println(numberName)
 
 
 
+    // Complex
+    val hourOfDay = 120
+    var timeOfTheDay : String = ""
+
+    timeOfTheDay = when (hourOfDay) {
+        0,1,2,3,4,5 -> "Early Morning"
+        6,7,8,9,10,11 -> "Morning"
+        12,13,14,15,16 -> "Afternoon"
+        17,18,19 -> "Evening"
+        20,21,22,23 -> "Late Evening"
+        else -> "INVALID Hour"
+    }
+
+    println(timeOfTheDay)
 
     // Advanced When Expressions
+
+    val xhourOfDay = 11
+    var xtimeOfTheDay : String = ""
+
+    xtimeOfTheDay = when (xhourOfDay) {
+        in 0..5 -> "Early Morning"
+        in 6..11 -> "Morning"
+        in 12..16 -> "Afternoon"
+        in 17..19 -> "Evening"
+        in 20..23 -> "Late Evening"
+        else -> "INVALID Hour"
+    }
+
+    println(xtimeOfTheDay)
+
+    // Finding Even
+    when {
+        number % 2 == 0 -> println("$number is Even")
+        else -> "$number is Odd"
+    }
+
+    // Finding Origin in x,y,z axis
+    var x = 0
+    var y = 0
+    var z = 3
+
+    when {
+        x == 0 && y == 0 && z == 0 -> println("Origin")
+        y ==0 && z == 0 -> println("On the X-Axis at x = $x")
+        x ==0 && z == 0 -> println("On the Y-Axis at y = $y")
+        x ==0 && y == 0 -> println("On the Z-Axis at z = $z")
+        else -> println("Somewhere in space at x = $x, y = $y and z = $z")
+    }
+
+    // More Complex
+    when {
+        x == y -> println("Along the y = x line.")
+        y == x * x -> println("Along the y = x^2 line.")
+    }
 
     // >>> Exercises
 

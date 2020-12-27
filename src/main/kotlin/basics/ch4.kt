fa1bc9940771c6050c46ff404e7a4e988a742624
abubaker <mohammad_abubaker@yahoo.com>
2020-12-27 14:27:34 +0500

    Working on Booleans

--- src/main/kotlin/basics/ch4.kt
+++ src/main/kotlin/basics/ch4.kt
@@ -10,7 +10,41 @@ fun main(){
      *****************************************    Boolean
      */
 
+    val yes: Boolean = true
+    val no: Boolean = false
+
+    val yes_x = true
+    val no_x = false
+
     // Boolean Operators
+
+    /// == Equals to
+    val doesOneEqualsTwo = (1 == 2)
+    println("1 == 2 : $doesOneEqualsTwo")
+
+    /// != Not Equal
+    val doesOneNotEqualsTwo = (1 != 2)
+    println("1 != 2 : $doesOneNotEqualsTwo")
+
+    /// ! Not (Switch)
+    val alsoTrue = !(1 == 2)
+    println("!(1==2) : $alsoTrue")
+
+    /// > Greater than
+    val isOneGreaterThanTwo = (1 > 2)
+    println("1 > 2 : $isOneGreaterThanTwo")
+
+    /// < Less than
+    val isOneLessThanTwo = (1 < 2)
+    println("1 < 2 : $isOneLessThanTwo")
+
+    /// >= Greater than or Equals to
+    val isOneGreaterThanOrEqualsTwo = (1 >= 2)
+    println("1 >= 2 : $isOneGreaterThanOrEqualsTwo")
+
+    /// <= Less than or Equals to
+    val isOneLessThanOrEqualsTwo = (1 <= 2)
+    println("1 <= 2 : $isOneLessThanOrEqualsTwo")
 
     // Logic
 

30a948ef5ba4ccd611261a4dce1bed216a64749d
abubaker <mohammad_abubaker@yahoo.com>
2020-12-27 22:35:48 +0500

    Completed String equality

--- src/main/kotlin/basics/ch4.kt
+++ src/main/kotlin/basics/ch4.kt
@@ -94,21 +94,104 @@ fun main(){
      *****************************************    If
      */
 
+    if (2 > 1) {
+        println("Yes, 2 is greater than 1.")
+    }
 
+    val animal = "Fox"
+    if (animal == "Cat" || animal == "Dog") {
+        println("Animal is a house pet")
+    } else {
+        println("Animal is not a house pet.")
+    }
+
+    val a = 5
+    val b = 10
+    val min = if ( a < b ) a else b
+    val max = if ( a > b ) a else b
+
+
+    val hourOfDay = 12
+    val timeOfDay = if(hourOfDay < 6) {
+        "Early Morning"
+    } else if(hourOfDay < 12) {
+       "Morning"
+    } else if(hourOfDay < 17) {
+        "Afternoon"
+    } else if(hourOfDay < 20) {
+        "Evening"
+    } else if(hourOfDay < 24) {
+        "Late Evening"
+    } else {
+        "Invalid Hour"
+    }
+    println(timeOfDay)
 
     // Short Circuiting
+    var name = "Matt"
+    if (1 > 2 && name == "Matt"){
+        println("It will be NEVER Printed, as both statements are not true")
+    }
 
-    // Encapsulating Variables
+    if (1 > 2 || name == "Matt"){
+        println("It will be Printed, as at least 1 statement is true")
+    }
+
+    // Encapsulating Variables (Scope)
+    var hoursWorked = 55
+
+    var price = 0
+
+    if(hoursWorked > 40) {
+        val hoursOver40 = hoursWorked - 40
+        price += hoursOver40 * 50
+        hoursWorked -= hoursOver40
+    }
+
+    price += hoursWorked * 25
+    println("Total Price $price")
 
     // >>> Exercises
+
+    // 1.
+    val myAgeT = 36
+    if(myAgeT > 13 && myAgeT < 19) {
+        println("You are a Teenager")
+    } else {
+        println("Sorry, are your not a teenager")
+    }
+
+    // 2.
+
 
     /*
      *****************************************    While
      */
 
-    // Repeat-While
+
+    // While
+    var sum = 1
+    while (sum < 10){
+        sum += sum + 1
+        println("Sum: $sum")
+    }
+
+    // Do-While
+    var sum_do = 1
+    do {
+        sum_do += sum_do + 1
+        println("Sum_DO: $sum_do")
+    } while (sum_do < 10)
 
     // Break
+    var sum_break = 1
+    while (true) {
+        sum_break += sum_break + 1
+        println("Sum_break: $sum_break")
+        if (sum_break >=10){
+            break
+        }
+    }
 
     // >>> Exercises
 

c0e84a5494037a36de5d94abae76bb5c2c9090de
abubaker <mohammad_abubaker@yahoo.com>
2020-12-28 16:46:03 +0500

    Working on functions...

--- src/main/kotlin/basics/ch6.kt
+++ src/main/kotlin/basics/ch6.kt
@@ -9,10 +9,43 @@ fun main(){
     /*
      *****************************************    Basics
      */
+    fun printMyName() {
+        println("My name is Mohammad Abubaker")
+    }
 
-    // Parameters
+    printMyName()
+
+    // Single Parameter
+    fun printMultipleOfFive(value: Int){
+        println("$value * 5 = ${value * 5}")
+    }
+    printMultipleOfFive(10)
+
+    // Two Parameters
+    fun printMultipleOf(multiplier: Int, andValue: Int) {
+        println("$multiplier * $andValue = ${multiplier * andValue}")
+    }
+    printMultipleOf(multiplier = 4, andValue = 3)
+
+    // Initial value (Fallback)
+    fun printXOf(multiplier: Int, value: Int = 1){
+        println("$multiplier * $value = ${multiplier * value}")
+    }
+    printXOf(4)
+
 
     // Return Values
+    fun multiply(number: Int, multiplier: Int) : Int {
+        return number * multiplier
+    }
+    println("Result: ${multiply(4,6)}")
+
+    fun multiplyAndDivide(number: Int, factor: Int): Pair<Int, Int>{
+        return Pair(number * factor, number / factor)
+    }
+    val (product, quotient) = multiplyAndDivide(4,2)
+    println("Product: $product, Quotient: $quotient")
+
 
     // Parameters as Values
 
